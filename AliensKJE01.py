2e50850a641c5e7f5ae6c20b4fba1e8da90f75d9
csPython <cspython@AD25TF04NGF1J-136.local>
2018-12-04 16:21:00 -0500

    while variables

---
+++ AliensKJE01.py
@@ -0,0 +1,15 @@
+# aliens.py with else
+#intered in the math
+#INIT variable
+l = int(0) #l =  aliens that have landed
+w = int(0) #w = Weeks that have passed
+week = str("week")
+lc = False
+wc = False
+
+
+# Intro
+print ("Today we will be calculating the amount of aliens that has a population corresponding to the amount of weeks.")
+print("How many aliens have landed on Earth?")
+
+
