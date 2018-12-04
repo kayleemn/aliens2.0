4d513afe9f0dc3dd535a42ab11ed18efc5f455d2
csPython <cspython@AD25TF04NGF1J-136.local>
2018-12-04 16:24:06 -0500

    second while loop

--- AliensKJE01.py
+++ AliensKJE01.py
@@ -1,5 +1,5 @@
 # aliens.py with else
-#entered in the math
+#intered in the math
 #INIT variable
 l = int(0) #l =  aliens that have landed
 w = int(0) #w = Weeks that have passed
@@ -18,7 +18,7 @@ while lc == False:
     try:
         l = int(input("please insert an amount of aliens that have landed on Earth"))
         if l > 0:
-             #insert a number that is greater than 0 to see the amount of aliens that landed on earth.
+             #Please insert a number that is greater than 0 to see the amount of aliens that landed on earth.
             print("How many weeks have passed?")
             lc = True
             #This checks to see if the user typed a number greater than 0.
@@ -26,6 +26,26 @@ while lc == False:
             print(" Try again, a positive number")
     except ValueError:
         print("only a number is excepted.")
-         # You only can escape the While loop if you ave typed a positive number.
+         # You only can escape the While loop if you have typed a positive number.
 
+while wc == False:
+    try:
+        w = int(input("please insert the amount of weeks that has passed"))
+          # insert the amount of weeks that passed.
+        if w > 0:
+            wc = True
+             #This checks to see if you have typed a positive number.
+        else:
+            print(" Try again, put in a number")
+            # Not escaping the loop.
+                 # Stays in loop unitl the user types the apporiate number.
+    except ValueError:
+          print("wrong number.")
+#math
+for i in range (w):
+    if (i == 1):
+        week = "weeks"
+        print("After", i + 1, week, "there will be", l*2**(i+1), " aliens on Earth.")
+         #You only get this line if you escape the loop.
+        
 
